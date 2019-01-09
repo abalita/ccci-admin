e6ad9fbb6a0c42516d6d7b560036f1a5af6292e7
Ariel Balita <ariel.a.balita@gmail.com>
2019-01-09 08:37:04 +0800

    update

--- app.js
+++ app.js
@@ -5,38 +5,17 @@ const port = 3000;
 
 
 app.get('/users', (req, res)=>{
-    res.json([
-        {
-            id:1,
-            name:'Balita, Ariel',
-            email:'ariel.a.balita@gmail.com',
-            avatar:'https://api.adorable.io/avatars/285/ariel.a.balita@gmail.com'
-        },
-        {
-            id:2,
-            name: 'Belo, Venus',
-            email:'venus.belo.rhi@gmail.com',
-            avatar:'https://api.adorable.io/avatars/285/venus.belo.rhi@gmail.com'
-        },
-        {
-            id:3,
-            name: 'Quijom, Mark Jhon Paul',
-            email:'markjhonpaul.quijom@gmail.com',
-            avatar:'https://api.adorable.io/avatars/285/markjhonpaul.quijom@gmail.com'
-        },
-        {
-            id:4,
-            name:'Quiocho, Chz',
-            email:'fquiocho@centralizedinc.com',
-            avatar:'https://api.adorable.io/avatars/285/fquiocho@centralizedinc.com'
-        },
-        {
-            id:5,
-            name:'Rivera, Godfrey',
-            email:'godofuri76@gmail.com',
-            avatar:'https://api.adorable.io/avatars/285/godofuri76@gmail.com'
+    res.json(users)
+})
+
+app.get('/users/:id', (req, res)=>{
+    var result = {};
+    users.forEach(user => {
+        if(user.id == req.params.id){
+            result = user;
         }
-    ])
+    })
+    res.json(result);
 })
 
 app.post('/users',(req, res)=>{
@@ -47,5 +26,38 @@ app.post('/users',(req, res)=>{
 app.get('/records', (req, res)=>{
 
 })
+
+var users = [
+    {
+        id:1,
+        name:'Balita, Ariel',
+        email:'ariel.a.balita@gmail.com',
+        avatar:'https://api.adorable.io/avatars/285/ariel.a.balita@gmail.com'
+    },
+    {
+        id:2,
+        name: 'Belo, Venus',
+        email:'venus.belo.rhi@gmail.com',
+        avatar:'https://api.adorable.io/avatars/285/venus.belo.rhi@gmail.com'
+    },
+    {
+        id:3,
+        name: 'Quijom, Mark Jhon Paul',
+        email:'markjhonpaul.quijom@gmail.com',
+        avatar:'https://api.adorable.io/avatars/285/markjhonpaul.quijom@gmail.com'
+    },
+    {
+        id:4,
+        name:'Quiocho, Chz',
+        email:'fquiocho@centralizedinc.com',
+        avatar:'https://api.adorable.io/avatars/285/fquiocho@centralizedinc.com'
+    },
+    {
+        id:5,
+        name:'Rivera, Godfrey',
+        email:'godofuri76@gmail.com',
+        avatar:'https://api.adorable.io/avatars/285/godofuri76@gmail.com'
+    }
+]
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}!`));
